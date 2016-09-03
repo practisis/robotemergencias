c9334da53f6f7a2efdc13f134cb9f884246f64c8
Practisis S.A <jeanpaul@practisis.com>
2016-09-03 17:40:55 -0500

    Create hook5.php

---
+++ hook5.php
@@ -0,0 +1,8 @@
+<?php
+
+$mibotAIML=1; //el id del bot AIML para este cliente.
+$clienteid=5; //el id del cliente para las funciones. 
+$token='126725331:AAFzk2tzZRSfpPgcDYilxCoJnQvzJ_A60Qo'; //el tocken de telegram.
+include "hookfinal.php";
+
+?>
